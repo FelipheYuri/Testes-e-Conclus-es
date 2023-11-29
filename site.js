b83c5797d26b8d043b13d686c93aa4d708023914
FelipheYuri <152447586+FelipheYuri@users.noreply.github.com>
2023-11-29 19:17:15 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,11 @@
+let x = 0;
+function botao1() {
+    x += 1;
+    document.getElementById("bt1").innerText = `+${x} Wood`;
+}
+function botao2() { 
+document.getElementById("bt2").innerHTML = "Gomu Gomu No Pistol!";
+}
+{
+  window.alert('Olá, seja bem-vindo(a) ao meu site!')
+}
